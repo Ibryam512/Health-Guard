97b3c5677e124c6ed8588a50c3ac844a445dbb0f
Ibryam512 <bamko2003@gmail.com>
2025-04-30 12:13:25 +0300

    Add rememberSavable for modal

--- app/src/main/java/com/example/healthguard/presentation/contacts/ContactsScreen.kt
+++ app/src/main/java/com/example/healthguard/presentation/contacts/ContactsScreen.kt
@@ -15,7 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +30,7 @@ fun ContactsScreen(
     viewModel: ContactsViewModel
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val openAlertDialog = remember { mutableStateOf(false) }
+    val openAlertDialog = rememberSaveable() { mutableStateOf(false) }
 
     Box(modifier = Modifier
         .fillMaxSize(),
